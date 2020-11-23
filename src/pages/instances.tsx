3699624e34e96bdfcee23e37753bd4ed9e96961d
Hamilton Chevez <hchev001@gmail.com>
2020-11-23 13:34:55 -0500

    working on adding the tiles of the instances

--- src/pages/instances.tsx
+++ src/pages/instances.tsx
@@ -1,16 +1,49 @@
 import React from "react"
 
 import { useStaticQuery, graphql } from "gatsby"
-import { Container } from "reactstrap"
+import { Container, Row, Col } from "reactstrap"
 import TemplateLayout from "../components/Layout/template-layout"
 import styled from "styled-components"
+import Img from "gatsby-image"
 
 const StyledMarkdown = styled.div``
-
+const InstanceCard = styled.div`
+  min-width: 80%;
+  background-color: ;
+`
 const Instances = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      AffiliatesImg: file(
+        relativePath: { eq: "sep-cyle-portfolio-tile.png" }
+      ) {
+        childImageSharp {
+          fluid() {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      DemoImg: file(relativePath: { eq: "sep-cyle-portfolio-tile.demo.png" }) {
+        childImageSharp {
+          fluid() {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
     <TemplateLayout title="Instances">
-      <Container></Container>
+      <Container>
+        <Row>
+          <Col>
+            <img src={ProdTile} />
+          </Col>
+          <Col>
+            <img src={DemoTile} />
+          </Col>
+        </Row>
+      </Container>
     </TemplateLayout>
   )
 }
