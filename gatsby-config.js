bc5745d80f7508ad23be272fc069574f5ee51239
Hamilton Chevez <hchev001@gmail.com>
2020-12-16 08:39:29 -0500

    changed site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `STEM-CyLE`,
+    description: `STEM-CyLE is a configurable learning and engagement cyberlearning environment that contains digital learning content in all areas of STEM.`,
     author: `@gatsbyjs`,
   },
   plugins: [
